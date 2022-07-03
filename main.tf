b68042b79b9ba1c7a1137ff8990dbd95e87f3bb5
una <unagamix@gmail.com>
2022-07-04 01:27:18 +0900

    feat: Lambda with API Gateway

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "policy" {
     effect = "Allow"
 
     principals {
-      identifiers = ["lambda.amazonaws.com"]
+      identifiers = ["lambda.amazonaws.com", "apigateway.amazonaws.com"]
       type        = "Service"
     }
 
@@ -29,26 +29,82 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.policy.json
 }
 
+resource "aws_iam_role" "iam_for_api_gateway" {
+  name               = "terraform-iam-for-api-gateway"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
+}
+
 # ===============================================
 # Policy
 # ===============================================
 
-resource "aws_iam_policy" "policy" {
-  name   = "terraform-iam-for-allow-policy"
-  policy = "${file("allow-policy.json")}"
+# ======================
+# Lambda
+# ======================
+
+resource "aws_iam_policy" "lambda_policy" {
+  name   = aws_iam_role.iam_for_lambda.name
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:UpdateItem",
+        "dynamodb:BatchWriteItem"
+      ],
+      "Resource": "arn:aws:dynamodb:*:*:table/*"
+    }
+  ]
+}
+EOF
 }
 
-resource "aws_iam_policy_attachment" "attach" {
-  name       = "terraform-iam-policy-attachment"
-  roles      = ["${aws_iam_role.iam_for_lambda.name}"]
-  policy_arn = "${aws_iam_policy.policy.arn}"
+resource "aws_iam_policy_attachment" "lambda_policy_attach" {
+  name       = "${aws_iam_role.iam_for_lambda.name}_attach"
+  policy_arn = aws_iam_policy.lambda_policy.arn
+  roles      = [
+    aws_iam_role.iam_for_lambda.name
+  ]
+}
+
+# ======================
+# API Gateway
+# ======================
+
+resource "aws_iam_policy" "api_gateway_policy" {
+  name   = aws_iam_role.iam_for_api_gateway.name
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": "lambda:InvokeFunction",
+          "Resource": "*"
+      }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "api_gateway_policy_attach" {
+  name       = "${aws_iam_role.iam_for_api_gateway.name}_attach"
+  roles      = ["${aws_iam_role.iam_for_api_gateway.name}"]
+  policy_arn = "${aws_iam_policy.api_gateway_policy.arn}"
 }
 
 # ===============================================
 # Lambda
 # ===============================================
 
-resource "aws_lambda_function" "lambda-1" {
+resource "aws_lambda_function" "get_user" {
   function_name = "terrform-getUser"
 
   filename         = "dist/index.zip"
@@ -59,7 +115,7 @@ resource "aws_lambda_function" "lambda-1" {
   runtime = "nodejs16.x"
 }
 
-resource "aws_lambda_function" "lambda-2" {
+resource "aws_lambda_function" "put_user" {
   function_name = "terrform-putUser"
 
   filename         = "dist/index.zip"
@@ -85,4 +141,89 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     name = "email"
     type = "S"
   }
+}
+
+# ===============================================
+# API Gateway
+# ===============================================
+
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name        = "terraform_example"
+  description = "'terraform_example' API Gateway"
+}
+
+resource "aws_api_gateway_resource" "get_user" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "get_user"
+}
+
+resource "aws_api_gateway_method" "get_user" {
+  rest_api_id      = aws_api_gateway_rest_api.rest_api.id
+  resource_id      = aws_api_gateway_resource.get_user.id
+  http_method      = "GET"
+  authorization    = "NONE"
+  api_key_required = false
+}
+
+resource "aws_api_gateway_method_response" "get_user" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.get_user.id
+  http_method = aws_api_gateway_method.get_user.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_user" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.get_user.id
+  http_method             = aws_api_gateway_method.get_user.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.get_user.invoke_arn
+  credentials             = aws_iam_role.iam_for_api_gateway.arn
+}
+
+resource "aws_api_gateway_integration_response" "get_user" {
+  depends_on = [
+    "aws_api_gateway_integration.get_user"
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.get_user.id
+  http_method = aws_api_gateway_method.get_user.http_method
+  status_code = aws_api_gateway_method_response.get_user.status_code
+
+  response_parameters ={
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+
+  depends_on = [
+    aws_api_gateway_integration.get_user
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "stg" {
+  stage_name    = "stg"
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
 }
